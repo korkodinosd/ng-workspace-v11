a9b55d7def5ffc71857eb7d255a1c3efe58ce49b
Korkodinos Dimitris <korkodinos_d@yahoo.gr>
2023-04-25 11:52:04 +0300

    Created the Shared Models Library

---
+++ projects/shared-models/src/lib/user.model.ts
@@ -0,0 +1,9 @@
+export interface UserModel {
+    id: string;
+    name: string;
+    lastname?: string;
+    salary: number;
+  }
+  
+  
+  export type UserRequiredProps = Pick<UserModel, 'name' | 'salary'>;
